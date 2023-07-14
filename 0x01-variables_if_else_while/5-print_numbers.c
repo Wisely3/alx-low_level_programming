f7d2ccca9e27c978ac3c24b2e66bca6f926dae80
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-14 14:20:23 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *Return: Always: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
