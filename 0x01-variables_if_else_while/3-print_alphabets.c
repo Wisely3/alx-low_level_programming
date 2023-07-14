307aadbfc2a0ff008345fcb94c727ca7ae2b3025
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-14 12:41:43 -0700

    Task 3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * A program that prints the alphabet
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
