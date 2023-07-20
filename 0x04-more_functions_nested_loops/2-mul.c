6a6b0934b3dfaf6eef66337a019c29f7c4c4d8e2
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-20 03:46:57 -0700

    Task 2. Collaboration is multiplaction

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function that multiplies two integers
+ * @a: Fisrt integer
+ * @b: Second integer
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	int ab  = a * b;
+
+	return (ab);
+}
