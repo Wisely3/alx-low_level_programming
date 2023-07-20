ab5403c3ab12e9ec2dad4063b6b23aadcb44ae25
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-20 05:20:40 -0700

    Task 7. I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line on the terminal
+ * @n: Is the input variable
+ *
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	int a;
+	int b;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (a = 0; a < n; a++)
+	{
+		for (b = 0; b < a; b++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
