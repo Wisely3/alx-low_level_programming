d34ebf1ffeca6dbb8420d57d9d013ebac09275a8
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-27 03:23:20 -0700

    Task 12. 104-print_buffer

---
+++ 0x06-pointers_arrays_strings/104-print_bu.c
@@ -0,0 +1,47 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - prints a buffer
+ * @b: number of bytes
+ * @size: size of the byte
+ */
+
+void print_buffer(char *b, int size)
+{
+	int p = 0, q;
+
+	if (size < 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	while (p < size)
+	{
+		if (p % 10 == 0)
+			printf("%08x: ", p);
+		for (q = p; q < p + 9; q += 2)
+		{
+			if ((q < size) && ((q + 1) < size))
+				printf("%02x%02x: ", b[q], b[q + 1]);
+			else
+			{
+				while (++q <= p + 10)
+					printf("  ");
+				printf(" ");
+			}
+		}
+		for (q = p; q < p + 9 && q < size; q++)
+		{
+			if (b[q] >= 32 && b[q] <= 126)
+				printf("%c", b[q]);
+			else
+				printf(".");
+		}
+		printf("\n");
+		p += 10;
+	}
+}
+
+
--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -5,42 +5,46 @@
  * print_buffer - prints a buffer
  * @b: number of bytes
  * @size: size of the byte
- * Return: 0
  */
 
 void print_buffer(char *b, int size)
 {
-	int p = 0, q;
+	int d, q, p;
 
-	if (size < 0)
+	d = 0;
+
+	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
-	while (p < size)
+	while (d < size)
 	{
-		if (p % 10 == 0)
-			printf("%08x: ", p);
-		for (q = p; q < p + 9; q += 2)
+		q = size - d < 10 ? size - d : 10;
+		printf("%08x: ", d);
+		for (p = 0; p < 10; p++)
 		{
-			if ((q < size) && ((q + 1) < size))
-				printf("%02x%02x: ", b[q], b[q + 1]);
+			if (p < q)
+				printf("%02x", *(b + d + p));
 			else
+				printf("  ");
+			if (p % 2)
 			{
-				while (++q <= p + 10)
-					printf(" ");
 				printf(" ");
 			}
 		}
-		for (q = p; q < p + 9 && q < size; q++)
+		for (p = 0; p < q; p++)
 		{
-			if (b[q] >= 32 && b[q] <= 126)
-				printf("%c", b[q]);
-			else
-				printf(".");
+			int c = *(b + d + p);
+
+			if (c < 32 || c > 132)
+			{
+				c = '.';
+			}
+			printf("%c", c);
 		}
 		printf("\n");
-		p += 10;
+		d += 10;
 	}
 }
 
