cd5a91c5873cc7ad59f15639603ff70a77e6b7e0
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-27 16:36:09 -0700

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,8 +8,19 @@
 
 void rev_string(char *s)
 {
-}
+	int a = 0, index = 0;
+       	char temp;
 
+	while (s[index++])
+		a++;
+
+	for (index = a - 1; index >= a / 2; index--)
+        {
+		temp = s[index];
+                s[index] = s[a - index - 1];
+                s[a - index - 1] = temp;
+        }
+}
 /**
  * infinite_add - adds two numbers
  * @n1: input number
