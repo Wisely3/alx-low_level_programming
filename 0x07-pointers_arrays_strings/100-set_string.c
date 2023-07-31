bb08cec159d0f4803a55be9be26e64bf79a2cc4e
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-31 10:31:57 -0700

    Task 8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string -  sets the value of a pointer to a char
+ * @s: input pointer
+ * @to: input char
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
