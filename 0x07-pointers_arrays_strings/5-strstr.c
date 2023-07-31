4c875e3fb24467a53f25bcace9ddd10eb5227dc1
Wyson Maleta <wysonmaleta@gmail.com>
2023-07-31 09:32:50 -0700

    Task.strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: input string
+ * @needle: input substring
+ *
+ * Return: pointer to needle, otherwise NULL
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int j;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		j = 0;
+
+		if  (haystack[j] == needle[j])
+		{
+			do {
+				if (needle[j + 1] == '\0')
+					return (haystack);
+				j++;
+			} while (haystack[j] == needle[j]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
+
+
+
