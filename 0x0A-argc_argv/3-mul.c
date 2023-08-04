e3b97839e9296f703ab3e10dd5beaec340f02e14
Wyson Maleta <wysonmaleta@gmail.com>
2023-08-04 10:37:39 -0700

    Task 3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,25 +4,25 @@
 
 /**
  * main - multiplies two numbers
- * @argc: the count of the argument 
+ * @argc: the count of the argument
  * @argv: the vector of the argument
  *
  * Return: Always 0 if success
  */
 
 int main(int argc, char *argv[])
-{       
-        int p = 0, q = 0;
-        
-        if (argc == 3)
-        {       
-                p = atoi(argv[1]);
-                q = atoi(argv[2]);
-                printf("%d\n", p * q);
-        }       
-        else
-        {       
-                printf("Error\n");
+{
+	int p = 0, q = 0;
+
+	if (argc == 3)
+	{
+		p = atoi(argv[1]);
+		q = atoi(argv[2]);
+		printf("%d\n", p * q);
+	}
+	else
+	{
+		printf("Error\n");
 		return (1);
 	}
 
