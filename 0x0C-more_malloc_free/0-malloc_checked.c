6322d1a939b2a4b0848a25dd95c3351bea0b398e
Wyson Maleta <wysonmaleta@gmail.com>
2023-08-10 07:00:05 -0700

    Task 0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: the number of bytes to allocate
+ *
+ * Return: pointer ptr to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
