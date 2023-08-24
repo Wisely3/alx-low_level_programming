f94078892d3045e87bf7018afc95c8ebc2625906
Wyson Maleta <wysonmaleta@gmail.com>
2023-08-24 15:42:13 -0700

    Commits for 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,13 +18,13 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (h->str == NULL)
-		{
+
 			printf("[0] (nil)\n");
-		}
+
 		else
-		{
+
 			printf("[%d] %s\n", h->len, h->str);
-		}
+
 		new_number++;
 		h = h->next;
 	}
